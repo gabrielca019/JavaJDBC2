628227ccaa445c56184280f1f9c6fede961a35d8
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2022-08-31 14:38:34 -0300

    Implementação da classe Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		Department dp = new Department(1, "Kleber");
-		System.out.println(dp);
+		Seller sl = new Seller(21, "Pereira", "pereir@gmail.com", new Date(), 3000.0, dp);
+		System.out.println(sl);
 	}
 
 }
